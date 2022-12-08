975987acaf77b1d19ff9c0627ee9ee567848d95c
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-08 20:18:20 +0200

    add

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoriesItem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItem.length}`);
+categoriesItem.forEach(function (category) {
+  console.log(`Category: ${firstElemethCild.textContent}`);
+  const categoriesTypeAmmount = category.lastElementChild.children.length;
+  console.log(`Elements: ${categoriesTypeAmmount}`);
+});
