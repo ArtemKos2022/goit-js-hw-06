efcd64409a7ae995c42cf1b897bf843c8959a589
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-09 21:43:31 +0200

    task-2

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const categoriesItem = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesItem.length}`);
 categoriesItem.forEach(function (category) {
-  console.log(`Category: ${firstElemethCild.textContent}`);
+  console.log(`Category: ${category.firstElementChild.textContent}`);
   const categoriesTypeAmmount = category.lastElementChild.children.length;
   console.log(`Elements: ${categoriesTypeAmmount}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients")
+
+const items = ingredients.map(listItemName => {
+  const listItem = document.createElement("li")
+  listItem.textContent = listItemName;
+  listItem.classList.add("item")
+  return listItem;
+});
+list.append(...items);
