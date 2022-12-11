3c8042d231d0b628bf0a1eaaa53f6eaa049dfe22
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-11 15:16:49 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const colorChangerBtn = document.querySelector(".change-color")
-const body = document.querySelector("body")
-const colorCode = document.querySelector(".color")
+const colorChangerBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorCode = document.querySelector('.color');
 const onGetcolor = () => {
   body.style.backgroundColor = getRandomHexColor();
   colorCode.textContent = getRandomHexColor();
-}
-colorChangerBtn.addEventListener("click", onGetcolor)
+};
+colorChangerBtn.addEventListener('click', onGetcolor);
